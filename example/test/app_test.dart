394225f0ca45af0c1c79d515bc362805527cc97c
guruliciousjide@gmail.com <guruliciousjide@gmail.com>
2019-03-31 18:43:45 +0100

    Added Travis

---
+++ example/test/app_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  testWidgets('app should work', (tester) async {
+    // Build an App with a Text Widget that displays the letter 'H'
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Text('H'),
+      ),
+    ));
+
+    // Find a Widget that displays the letter 'H'
+    expect(find.text('H'), findsOneWidget);
+  });
+}
